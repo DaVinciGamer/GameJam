065cb2b2dd7577ac05faa92b5baf1c8fc6c1dd86
JulianSeibert99 <julian@rseibert.de>
2024-07-16 18:11:28 +0200

    LifeBar funktioniert jetzt  + UI Anpassung + EnemyDamage individuell einstellbar

--- My project/Assets/Scripts/PlayerCollision.cs
+++ My project/Assets/Scripts/PlayerCollision.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerClass playerClass;
+    public Slider healthSlider;
+    public EnemyDamage enemyDamage;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            playerClass.currentHealth -= 10;
+            // Reduziere die Gesundheit des Spielers
+            playerClass.currentHealth -= enemyDamage.damageAmount;
+
+            // Aktualisiere den Slider-Wert
+            healthSlider.value = (float)playerClass.currentHealth / playerClass.maxHealth;
         }
     }
 }
